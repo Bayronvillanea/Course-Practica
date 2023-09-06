f106bbef8af20dfb280c472410bed3e3262bf3bd
Bayronvillanea <bayronvillaneahidalgo.30@gmail.com>
2023-09-05 22:24:10 -0600

    fin practica

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'bayronvillanea3',
+    name: 'Bayron Villanea',
+    isFollowing: true
+  },{
+    userName: 'S4vitar',
+    name: 'S4vitar',
+    isFollowing: true
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
